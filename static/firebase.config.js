d7a46ed760b69f72c2a0903423449fcb505d761e
Manny Manufacture <MannyManufacture@OutLook.com>
2025-08-22 03:40:00 +0300

    Delete static directory

--- static/firebase.config.js
+++
@@ -1,9 +0,0 @@
-window.FIREBASE_CONFIG = {
-  apiKey: "AIzaSyC-YyXtXymBYZobes7pm1FfEFJM2NgJhiU",
-  authDomain: "mannymanufacture.firebaseapp.com",
-  projectId: "mannymanufacture",
-  storageBucket: "mannymanufacture.appspot.com",
-  messagingSenderId: "264193789436",
-  appId: "1:264193789436:web:e6bb6e40724a1c8dd9f679",
-  measurementId: "G-3HSZCC5850"
-};
